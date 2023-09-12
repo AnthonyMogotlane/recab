a6983029120cd2acf2247572d5dfacdbf9fcfd94
Anthony Mogotlane <almogotlane@gmail.com>
2023-09-12 12:22:25 +0200

    Added auth, routes and components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import './App.css';
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Authenticator, Flex } from "@aws-amplify/ui-react";
 
-import '@aws-amplify/ui-react/styles.css';
-import { Card, Flex, Image, Badge, Text, StepperField, Button } from '@aws-amplify/ui-react';
-
+import { Login } from "./components/Login";
+import { RequireAuth } from "./components/RequireAuth";
+import HomePage from "./pages/home";
+import NavBar from "./components/NavBar";
+import AboutPage from "./pages/about";
+import Users from "./pages/users";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Card variation="elevated" width="50%" margin="auto" >
-      <Flex alignItems="flex-center" justifyContent="center" >
-        <Text>RECAB</Text>
-      </Flex>
-    </Card>
+    <Authenticator.Provider>
+      <BrowserRouter>
+        <Flex direction="column" position="relative" minHeight="100vh">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route
+              path="/users"
+              element={
+                <RequireAuth>
+                  <Users />
+                </RequireAuth>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <Footer />
+        </Flex>
+      </BrowserRouter>
+    </Authenticator.Provider>
   );
 }
 
