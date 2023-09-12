1d83b27ad9886d09b3a64a49f6e63f736d963bd7
Anthony Mogotlane <almogotlane@gmail.com>
2023-09-12 12:21:00 +0200

    Added amplify config to interact with backend services

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+//Amplify config to interact with backend services.
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
