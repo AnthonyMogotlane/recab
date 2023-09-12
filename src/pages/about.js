7b1aa4210e3089fa1bd545ade5880c24b2001070
Anthony Mogotlane <almogotlane@gmail.com>
2023-09-12 12:18:22 +0200

    Added about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Heading, View, Text } from '@aws-amplify/ui-react';
+
+const AboutPage = () => {
+  return (
+    <View textAlign="center">
+      <Heading>RECAB</Heading>
+      <Text>Recab simply stand for Recommend A Book</Text>
+    </View>
+  )
+}
+
+export default AboutPage;
